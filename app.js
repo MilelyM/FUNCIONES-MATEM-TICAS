85288614fe1d909d5ec7ca6c28eedf6c42923cef
milelym <milelymarcial@gmail.com>
2018-05-27 16:40:01 -0300

    funciones

---
+++ app.js
@@ -0,0 +1,15 @@
+function add(a,b){
+
+    return (a+b);
+}
+   
+function multiply(a,b){
+    return a*b;
+}
+   
+console.log("Resultado 2: " + multiply(36325,(9824 + 777)));
+console.log("Resultado 1: " + add(9824,777));
+document.writeln("FUNCIONES MATEMÁTICAS<br> Resultado 1: "+ add(9824,777));
+document.writeln("<p>FUNCIONES MATEMÁTICAS<br> Resultado 2: "+ multiply(36325,(9824 + 777))+"</p>"); 
+
+
