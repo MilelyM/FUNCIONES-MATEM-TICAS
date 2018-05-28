b609f64f0ad797968a6917a916124ceb81392cc7
milelym <milelymarcial@gmail.com>
2018-05-28 00:09:18 -0300

    ordenar cambios

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ function add(a,b){
 function multiply(a,b){
     return a*b;
 }
-   
+let suma= add(9824,777)
 console.log("Resultado 2: " + multiply(36325,(9824 + 777)));
 console.log("Resultado 1: " + add(9824,777));
-document.writeln("FUNCIONES MATEMÁTICAS<br> Resultado 1: "+ add(9824,777));
-document.writeln("<p>FUNCIONES MATEMÁTICAS<br> Resultado 2: "+ multiply(36325,(9824 + 777))+"</p>"); 
+
+document.writeln("<p>FUNCIONES MATEMÁTICAS<br> Resultado : "+ multiply(36325,(suma))+"</p>"); 
 
 
